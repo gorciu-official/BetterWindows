36e22aaee2d5497666bbb7bd4545909e1c8e8998
Gorciu <gorciuyt@gmail.com>
2025-01-22 17:55:14 +0100

    Some checksum shit

--- online-content/ActualInstaller.ps1
+++ online-content/ActualInstaller.ps1
@@ -8,3 +8,18 @@ cmd.exe /c "timeout 5 > nul"
 
 Write-Host "Adding BetterWindows to the PATH variable..." -ForegroundColor Yellow
 $env:Path = $env:Path + ";C:\BetterWindows"
+Write-Host "Downloading `winman` executable..." -ForegroundColor Yellow
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TheBetterWindows/BetterWindows/refs/heads/main/online-content/winman.binary.exe" -OutFile "C:\BetterWindows\temp\winman.exe"
+Write-Host "Checking checksum..." -ForegroundColor Yellow
+$checksum = Get-FileHash "C:\BetterWindows\temp\winman.exe" -Algorithm SHA256
+$expectedChecksum = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TheBetterWindows/BetterWindows/refs/heads/main/online-content/winman.checksum-sha256.txt").Content.Trim()
+if ($checksum.Hash -eq $expectedChecksum) {
+    Write-Host "Checksum is valid. Continuing installation..." -ForegroundColor Green
+} else {
+    Write-Host "Checksum is invalid. Please close BetterWindows installation." -ForegroundColor Red
+    while ($true) {
+        Start-Sleep -Seconds 5
+    }
+}
+
+Write-Host "BetterWindows has been successfully installed!" -ForegroundColor Green
